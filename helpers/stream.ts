363cf3d509c6e5e2e6055ffb9df4b2269c217d58
unnameduser95 <unnameduser95@pm.me>
2021-05-21 11:31:24 -0700

    Test streaming transcription

--- helpers/stream.ts
+++ helpers/stream.ts
@@ -1,6 +1,5 @@
 // Helper file for handling user's audio streams.
 import Discord from 'discord.js';
-import fs from 'fs';
 
 import gSpeech from '@google-cloud/speech';
 import { LiveTranscript } from './liveTranscript';
@@ -9,7 +8,16 @@ const speechClient = new speech.SpeechClient({
   keyFilename: 'stt-gcloud-key.json'
 });
 
-function handleConnection(connection: Discord.VoiceConnection, liveTranscript: LiveTranscript) {
+/**
+ * Handle voice connections and live transcript updates.
+ * @param connection 
+ * @param liveTranscript 
+ * @returns Promise that resolves when bot is disconnected from VC.
+ */
+function handleConnection(
+  connection: Discord.VoiceConnection, 
+  liveTranscript: LiveTranscript
+) {
   const promise = new Promise(function(resolve, reject) {
     connection.on('speaking', function(user, speaking) {
       if (speaking.bitfield === 0 || user.bot) {
@@ -17,10 +25,33 @@ function handleConnection(connection: Discord.VoiceConnection, liveTranscript: L
       };
   
       console.log(`Listening to user ${user.tag}`);
+
+      // Transcribe a stream using the Google Speech client
+      const recognizeStream = speechClient.streamingRecognize({
+        config: {
+          audioChannelCount: 2,
+          encoding: "LINEAR16",
+          languageCode: "en-US",
+          sampleRateHertz: 48000
+        },
+        interimResults: true
+      })
+        .on('readable', () => {
+          console.log("Receiving data");
+        })
+        .on('error', console.error)
+        .on('data', data => {
+          process.stdout.write(
+            data.results[0] && data.results[0].alternatives[0]
+              ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+              : '\n\nReached transcription time limit, press Ctrl+C\n'
+          );
+        });
   
       const stream = connection.receiver.createStream(user, {
         mode: 'pcm'
       });
+      stream.pipe(recognizeStream);
 
       let bufferArray: Array<Buffer> = [];
   
@@ -34,24 +65,26 @@ function handleConnection(connection: Discord.VoiceConnection, liveTranscript: L
   
       stream.on('end', function() {
         const buffer = Buffer.concat(bufferArray);
-        convertAudio(buffer)
-          .then(newBuffer => {
-            return transcribe(newBuffer);
-          })
-          .then(transcript => {
-            if (transcript) {
-              console.log(`${user.tag}: ${transcript}`);
+        
+        // Disable buffer conversion to save on Google Cloud credits
+        // convertAudio(buffer)
+        //   .then(newBuffer => {
+        //     return transcribe(newBuffer);
+        //   })
+        //   .then(transcript => {
+        //     if (transcript) {
+        //       // TO-DO: Fix crash if message doesn't exist
+        //       console.log(`${user.tag}: ${transcript}`);
 
-              try {
-                liveTranscript.addOrUpdateTranscript(user, transcript);
-                liveTranscript.refresh();
-              } catch(err) {
-                console.error(err);
-              }
-              
-            }
+        //       try {
+        //         liveTranscript.addOrUpdateTranscript(user, transcript);
+        //         liveTranscript.refresh();
+        //       } catch(err) {
+        //         console.error(err);
+        //       }
+        //     }
             
-          });
+        //   });
 
         console.log(`Audio stream closed for user ${user.tag}`);
       })
@@ -74,59 +107,33 @@ function handleConnection(connection: Discord.VoiceConnection, liveTranscript: L
   return promise;
 };
 
-// async function transcribeFileStream(userId: string) {
-//   const config = {
-//     encoding: "LINEAR16",
-//     sampleRateHertz: 48000,
-//     languageCode: "en-US"
-//   };
+/**
+ * Handle transcript updates.
+ * @param user 
+ * @param transcript 
+ * @param liveTranscript 
+ */
+async function handleTranscript(
+  user: Discord.User, 
+  transcript: string, 
+  liveTranscript: LiveTranscript
+) {
+  // TO-DO: Add socket event for transcript updates (public API)
 
-//   const request = {
-//     config: config,
-//     interimResults: true
-//   };
+  // update the message
+  try {
+    liveTranscript.addOrUpdateTranscript(user, transcript);
+    
+    // delay for message updates (affects how often message is updated)
+    const delay = process.env.MESSAGE_DELAY ? Number(process.env.MESSAGE_DELAY) : undefined;
 
-//   // @ts-ignore
-//   const recognizeStream = speechClient.streamingRecognize(request)
-//     .on('readable', () => {
-//       console.log("Receiving data");
-//     })
-//     .on('error', console.error)
-//     .on('data', data => {
-//       console.log("Received data");
-//       console.log(data);
-//       // process.stdout.write(
-//       //   data.results[0] && data.results[0].alternatives[0]
-//       //     ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-//       //     : '\n\nReached transcription time limit, press Ctrl+C\n'
-//       // )
-//     })
-
-//   fs.createReadStream(userId).pipe(recognizeStream);
-// }
-
-// async function transcribeFile(userId: string) {
-//   const audio = {
-//     content: fs.readFileSync(userId).toString('base64')
-//   };
-
-//   // console.log(audio);
-
-//   const [response] = await speechClient.recognize({
-//     config: {
-//       encoding: 'LINEAR16',
-//       sampleRateHertz: 48000,
-//       languageCode: 'en-US',
-//       model: 'video',
-//     },
-//     audio: audio
-//   });
-
-//   const transcription = response.results
-//     .map(result => result.alternatives[0].transcript)
-//     .join('\n');
-//   console.log('Transcription: ', transcription);
-// };
+    if (liveTranscript.lastUpdate.getTime() > (delay && !isNaN(delay) ? delay : 1000)) {
+      liveTranscript.refresh();
+    }
+  } catch(err) {
+    throw new Error(err);
+  }
+}
 
 async function convertAudio(input: Buffer) {
   try {
@@ -145,6 +152,11 @@ async function convertAudio(input: Buffer) {
   }
 };
 
+/**
+ * Transcribe a buffer using the Google Speech client.
+ * @param buffer 
+ * @returns The transcript returned by the client.
+ */
 async function transcribe(buffer: Buffer) {
   try {
     const bytes = buffer.toString('base64');
