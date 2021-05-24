b6dd3c1884f474134a7580640a53834eb403ed1c
Brendan <brendann.ch@gmail.com>
2021-05-23 23:47:20 -0700

    Add support for API key system

--- registerSocket.ts
+++ registerSocket.ts
@@ -1,5 +1,9 @@
+import bcrypt from 'bcrypt';
+
 import { Server, Socket } from 'socket.io';
 import { getLiveTranscripts } from './helpers/liveTranscript';
+import { findServer } from './helpers/server';
+
 const io = new Server();
 
 /**
@@ -7,10 +11,38 @@ const io = new Server();
  * @param socket 
  */
 function registerSocket(socket: Socket) {
-  socket.on('transcript:subscribe', function(serverId: string) {
+  socket.on('transcript:subscribe', async function(serverId?: string, apiKey?: string) {
+    if (!serverId || !apiKey) {
+      const error = "No server ID or API key specified.";
+      socket.emit('error', error);
+
+      return;
+    }
+
     console.log(`Socket ${socket.id} subscribing to updates from server ${serverId}`);
 
     // TO-DO: check if server ID matches up with authentication
+    const server = await findServer(serverId);
+
+    const matchArray: Array<Promise<boolean>> = server.keys.map(key => {
+      return bcrypt.compare(apiKey, key);
+    })
+
+    const matchResults = await Promise.all(matchArray);
+    if (!matchResults.includes(true)) {
+      socket.emit('error', "API key is invalid.");
+
+      return;
+    };
+    // const keyToCheck = await bcrypt.hash(apiKey, 10);
+    // console.log(keyToCheck);
+
+    // check if API key exists in server keys
+    // if (!server.keys.includes(keyToCheck)) {
+    //   socket.emit('error', "API key is invalid.");
+      
+    //   return;
+    // };
 
     const liveTranscript = getLiveTranscripts().find(
       transcript => (transcript.message ? transcript.message.guild!.id : null) === serverId
@@ -21,7 +53,7 @@ function registerSocket(socket: Socket) {
 
   socket.on('disconnect', (reason) => {
     const liveTranscript = getLiveTranscripts().find(
-      transcript => transcript.socket ? transcript.socket.id : null === socket.id
+      transcript => (transcript.socket ? transcript.socket.id : null) === socket.id
     );
 
     liveTranscript?.removeSocket();
@@ -29,7 +61,7 @@ function registerSocket(socket: Socket) {
 }
 
 function registerConnection() {
-  console.log("Registering connection....");
+  console.log("Registering socket connection....");
 
   io.on('connection', (socket: Socket) => {
     console.log(`Socket ${socket.id} connected.`);
